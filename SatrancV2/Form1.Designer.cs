2a01af92f768ce5b66ea7cc05813b2743ee21a6e
Mustafa Aşkın İNAN <mustafaaskininan@gmail.com>
2019-12-22 03:38:21 +0300

    Satranc At Oyunu

---
+++ SatrancV2/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SatrancV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbBoyut = new System.Windows.Forms.ComboBox();
+            this.btnCiz = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbBoyut
+            // 
+            this.cmbBoyut.FormattingEnabled = true;
+            this.cmbBoyut.Items.AddRange(new object[] {
+            "5x5",
+            "6x6",
+            "7x7",
+            "8x8",
+            "9x9"});
+            this.cmbBoyut.Location = new System.Drawing.Point(375, 455);
+            this.cmbBoyut.Name = "cmbBoyut";
+            this.cmbBoyut.Size = new System.Drawing.Size(59, 21);
+            this.cmbBoyut.TabIndex = 5;
+            this.cmbBoyut.Text = "Seçiniz";
+            // 
+            // btnCiz
+            // 
+            this.btnCiz.Location = new System.Drawing.Point(375, 482);
+            this.btnCiz.Name = "btnCiz";
+            this.btnCiz.Size = new System.Drawing.Size(59, 23);
+            this.btnCiz.TabIndex = 4;
+            this.btnCiz.Text = "Çiz";
+            this.btnCiz.UseVisualStyleBackColor = true;
+            this.btnCiz.Click += new System.EventHandler(this.btnCiz_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 511);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Sıfırla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 543);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbBoyut);
+            this.Controls.Add(this.btnCiz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbBoyut;
+        private System.Windows.Forms.Button btnCiz;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SatrancV2/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SatrancV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int boyut = 0;
+        private void btnCiz_Click(object sender, EventArgs e)
+        {
+            btnCiz.BackColor = Color.White;
+
+            if (cmbBoyut.Text == "5x5")
+            {
+                boyut = 7;
+                btnCiz.Enabled = false;
+            }
+            else if (cmbBoyut.Text == "6x6")
+            {
+                boyut = 8;
+                btnCiz.Enabled = false;
+            }
+            else if (cmbBoyut.Text == "7x7")
+            {
+                boyut = 9;
+                btnCiz.Enabled = false;
+            }
+            else if (cmbBoyut.Text == "8x8")
+            {
+                boyut = 10;
+                btnCiz.Enabled = false;
+            }
+            else if (cmbBoyut.Text == "9x9")
+            {
+                boyut = 11;
+                btnCiz.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Lütfen değer seçiniz...", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+
+            for (int i = 0; i < boyut; i++)
+            {
+                for (int j = 0; j < boyut; j++)
+                {
+                    string x = Convert.ToString(i);
+                    string y = Convert.ToString(j);
+                    string z = x + y;
+                    string k, l, m, n;
+                    k = Convert.ToString(i) + "0";
+                    l = "0" + Convert.ToString(j);
+                    m = Convert.ToString(i) + Convert.ToString(boyut - 1);
+                    n = Convert.ToString(boyut - 1) + Convert.ToString(j);
+                    Button b = new Button();
+                    b.Top = i * 40;
+                    b.Left = j * 40;
+                    b.Width = 40;
+                    b.Height = 40;
+                    b.Name = z;
+                    b.BackColor = Color.White;
+                    b.Click += btnClick;
+                    this.Controls.Add(b);
+                    if (b.Name == k || b.Name == l || b.Name == m || b.Name == n)
+                    {
+                        b.BackColor = Color.Black;
+                        b.Enabled = false;
+                    }
+                }
+            }
+        }
+        private void btnClick(object sender, EventArgs e)
+        {
+            Button b = ((Button)(sender));
+
+            b.Enabled = false;
+            b.BackColor = Color.DarkBlue;
+            int aktarıcı = Convert.ToInt32(b.Name) - 19;
+            string hamle1, hamle2, hamle3, hamle4, hamle5, hamle6, hamle7, hamle8;
+            hamle1 = Convert.ToString(aktarıcı);
+            aktarıcı = aktarıcı - 2;
+            hamle2 = Convert.ToString(aktarıcı);
+            aktarıcı = aktarıcı + 9;
+            hamle3 = Convert.ToString(aktarıcı);
+            aktarıcı = aktarıcı + 4;
+            hamle4 = Convert.ToString(aktarıcı);
+            aktarıcı = aktarıcı + 16;
+            hamle5 = Convert.ToString(aktarıcı);
+            aktarıcı = aktarıcı + 4;
+            hamle6 = Convert.ToString(aktarıcı);
+            aktarıcı = aktarıcı + 7;
+            hamle7 = Convert.ToString(aktarıcı);
+            aktarıcı = aktarıcı + 2;
+            hamle8 = Convert.ToString(aktarıcı);
+
+            Form1 frm = null;
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.Text == "Form1")
+                {
+                    frm = (Form1)f;
+                }
+            }
+
+            if (frm != null)
+            {
+                foreach (Control c in frm.Controls)
+                {
+                    if (c is Button)
+                    {
+                        if (c.BackColor == Color.White)
+                        {
+                            c.Enabled = false;
+                        }
+                        if (c.BackColor == Color.DeepSkyBlue)
+                        {
+                            c.BackColor = Color.White;
+                            c.Enabled = false;
+                        }
+                        if (c.Name == hamle2 || c.Name == hamle1 || c.Name == hamle3 || c.Name == hamle4 || c.Name == hamle5 || c.Name == hamle6 || c.Name == hamle7 || c.Name == hamle8 )
+                        {
+
+                            if (c.BackColor == Color.DarkBlue || c.BackColor == Color.Black)
+                            { continue; }
+                            
+                            c.BackColor = Color.DeepSkyBlue;
+                            c.Enabled = true;
+
+                            
+                        }
+                    }
+                }
+            }
+
+            if (frm != null)
+            {
+                int sayac = 0;
+                int sayac2 = 0;
+                foreach (Control c in frm.Controls)
+                {
+
+                    if (c is Button)
+                    {
+                        if (c.Enabled == false)
+                        {
+                            sayac++;
+                        }
+                        if (c.BackColor == Color.DarkBlue)
+                        {
+                            sayac2++;
+                        }
+                    }
+                }
+                if (sayac2 == (boyut - 2) * (boyut - 2))
+                {
+                    MessageBox.Show("Kazandınız!");
+                }
+                else if (sayac == boyut * boyut + 1)
+                { MessageBox.Show("Oyunu bitti.Skorunuz:" + sayac2); }
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < boyut; i++)
+            {
+                for (int j = 0; j < boyut; j++)
+                {
+                    string z = Convert.ToString(i) + Convert.ToString(j);
+                    Button b = new Button();
+                    Form1 frm = null;
+                    foreach (Form f in Application.OpenForms)
+                    {
+                        if (f.Text == "Form1")
+                        {
+                            frm = (Form1)f;
+                        }
+                    }
+
+                    if (frm != null)
+                    {
+                        foreach (Control c in frm.Controls)
+                        {
+                            if (c is Button)
+                            {
+                                if (c.Name == z)
+                                {
+                                    frm.Controls.Remove(c);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            btnCiz.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
